38891ec47cfa2ad1c904c9fa38189fb077d91c55
eeric <eeric@users.noreply.github.com>
2021-08-07 11:32:54 +0800

    Update inference.py

--- code/insightface/arcface_mxnet/inference.py
+++ code/insightface/arcface_mxnet/inference.py
@@ -23,17 +23,21 @@ def get_array(face_chip):
 # load mxnet weight
 prefix = "./models/insightface/model"
 sym, arg, aux = mx.model.load_checkpoint(prefix, 0)
+
 # define mxnet
 ctx = mx.cpu() # gpu_id = 0 ctx = mx.gpu(gpu_id)
 mod = mx.mod.Module(symbol=sym, context=ctx, label_names=None)
 mod.bind(for_training=False, data_shapes=[('data', (1,3,112,112))])
 mod.set_params(arg, aux)
 Batch = namedtuple('Batch', ['data'])
+
 # read image
 img = cv2.imread("face_chip.jpg")
 array = get_array(img)
+
 # inference
 mod.forward(Batch([array]))
+
 # feature
 feat = mod.get_outputs()[0].asnumpy()
 print(feat)
