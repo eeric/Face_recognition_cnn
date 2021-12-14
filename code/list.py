e2c223491646bfe29f3826a281fc23db865f33fd
eeric <eeric@users.noreply.github.com>
2021-12-14 10:33:29 +0800

    Update list.py

--- code/list.py
+++ code/list.py
@@ -12,6 +12,8 @@ https://github.com/moskomule/senet.pytorch/tree/master/senet
 https://github.com/YimianDai/open-aff/tree/0bcfd8a1b010a66075db6232b1141b35f0f07532/aff_pytorch
 # GB-CosFace
 https://github.com/aliyun/GB-CosFace
+# DREAM
+https://github.com/penincillin/DREAM
   
   
   
